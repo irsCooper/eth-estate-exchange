1745e4f4ed615b00338a5d520223c174cd6e0fd0
Cooper <118792324+irsCooper@users.noreply.github.com>
2024-05-02 13:16:54 +0400

    Update Exchange.sol

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -11,57 +11,90 @@ contract Contract {
 
     address Owner;
 
+    uint estateId = 1;
+    
     constructor(address estNft, address eston) {
         nft = EstNft(estNft);
         token = Eston(eston);
-        owner = msg.sender;
+        Owner = msg.sender;
     }
 
-
+    struct Estate { 
+        uint Id;
+        string Address;
+        string Desc;
+        string Haracteristic;
+    }
 
     struct Deal {
         uint Id;
-        uint EstateId;
+        Estate Estate;
         address OwnerEstate;
         address Arendator;
         uint Mounth;
         uint PriceOneMounth;
+        uint MounthPay;
         bool Active;
     }
 
-    mapping (address => Deal) userDeal; //сделки пользователя
-
     struct ArendsEstate {
         uint Id;
-        uint EstateId;
+        Estate Estate;
         address OwnerEstate;
         uint MinMounth;
         uint MaxMounth;
         uint PriceOneMounth;
     }
 
-    ArendsEstate[] arends;
-    mapping (uint => Deal) delFromArends; // заявки на аренду
-
-    
-
-
-    mapping (uint => bool) public estateSell;
-    mapping (address => mapping (uint => bool)) public estateArendator; //является ли пользователь арендатором недвижимости
-
-    mapping (address => uint) userArendsEstate; //число арендованных домов пользователя
-
-    function changeSellEstate(address owner, uint tokenId, bool status) public {
-        require(estateOwner[tokenId] == owner, "you are not owner this token");
-        estateSell[tokenId] = status;
+    struct RequestRent {
+        uint Id;
+        address Arendator;
+        uint Mounth;
     }
 
-    function getUserArendsEstate() public view returns(Estate[] memory) {
-        Estate[] memory es = new Estate[](userArendsEstate[msg.sender]);
+
+    mapping (uint => Estate) public estate;
+    mapping (uint => bool) estateSell;
+
+    mapping (uint => bool) estateRents; //арендуется ли в настоящий момент
+    
+    mapping (address => Deal[]) public userDeal; //сделки пользователя
+
+    ArendsEstate[] arends;
+    mapping (uint => RequestRent[]) requestRent;
+    
+    mapping (address => uint) userArendsEstate; //число арендованных домов пользователя
+    mapping (address => mapping (uint => bool)) public estateArendator; //является ли пользователь арендатором недвижимости
+    mapping (address => mapping (uint => uint)) public userEstateToDealId; //является ли пользователь арендатором недвижимости
+
+
+    modifier isEstate(uint tokenId) {
+        require(estate[tokenId].Id != 0, "invalid token");
+        _;
+    }
+
+    modifier CheckOwnerOfEstate(uint tokenId)  {
+        require(nft.OwnerOf(tokenId) == msg.sender, "this user is not owner for token");
+        _;
+    }
+
+
+
+
+
+    function setEstate(string memory _address, string memory _desc, string memory _haract) public {
+        uint tokenId = estateId++;
+
+        nft.mint(msg.sender, tokenId);
+        estate[tokenId] = Estate(tokenId, _address, _desc, _haract);
+    }
+
+    function getUserEstate() public view returns(Estate[] memory) {
+        Estate[] memory es = new Estate[](nft.BalanceOf(msg.sender));
         uint p;
 
-        for(uint i = 0; estateId > i; i++) {
-            if(estateArendator[msg.sender][i] == true) {
+        for(uint i = 1; estateId > i; i++) {
+            if(nft.OwnerOf(i) == msg.sender) {
                 es[p] = estate[i];
                 p++;
             }
@@ -69,4 +102,101 @@ contract Contract {
 
         return es;
     }
+
+    ///добавить на биржу, с целью арендовать кому-нибудь дом
+    function toAdrendsEstate(uint tokenId, uint min, uint max, uint price) public isEstate(tokenId) CheckOwnerOfEstate(tokenId) {
+        require(estateRents[tokenId] == false, "this estate already rents");
+
+        requestRent[arends.length].push(RequestRent(0, address(0), 0));
+
+        arends.push(ArendsEstate(
+            arends.length,
+            estate[tokenId],
+            msg.sender,
+            min,
+            max,
+            price
+        ));
+    }
+
+    function setRequestToRentEstate(uint _id, uint _mounth) public {
+        require(arends.length > _id, "invalid id");
+        ArendsEstate memory ae = arends[_id];
+        require(ae.MinMounth <= _mounth && ae.MaxMounth >= _mounth, "incorrect mounth of arend");
+        require(token._BalanceOf(msg.sender) >= ae.PriceOneMounth * _mounth, "not money");
+
+        requestRent[_id].push(RequestRent(requestRent[_id].length, msg.sender, _mounth));
+    }
+
+    function getArendsEstate() public view returns(ArendsEstate[] memory) {
+        return arends;
+    }
+
+    function getRequestToRentEstate(uint id) public view returns(RequestRent[] memory) {
+        return requestRent[id];
+    }
+
+    function setArendsEstate(uint idRent, uint idRequest) public {
+        require(idRequest < requestRent[idRent].length && idRequest > 0, "invalid id");
+        ArendsEstate memory ae = arends[idRent];
+        RequestRent memory req = requestRent[idRent][idRequest];
+        require(ae.OwnerEstate == msg.sender, "you are not owner this request");
+        
+        setUserArendsEstate(req.Arendator, ae.Estate.Id, true);
+        userEstateToDealId[msg.sender][ae.Estate.Id] = userDeal[msg.sender].length;
+        userDeal[msg.sender].push(Deal(
+            userDeal[msg.sender].length,
+            ae.Estate,
+            msg.sender,
+            req.Arendator,
+            req.Mounth,
+            ae.PriceOneMounth,
+            0,
+            true
+        ));
+
+
+        userEstateToDealId[req.Arendator][ae.Estate.Id] = userDeal[req.Arendator].length;
+        userDeal[req.Arendator].push(Deal(
+            userDeal[req.Arendator].length,
+            ae.Estate,
+            msg.sender,
+            req.Arendator,
+            req.Mounth,
+            ae.PriceOneMounth,
+            0,
+            true
+        ));
+
+        delete requestRent[idRent];
+        requestRent[idRent] = requestRent[arends.length - 1];
+        delete  requestRent[arends.length - 1];
+
+        arends[arends.length - 1].Id = idRent;
+        arends[idRent] = arends[arends.length - 1];
+        arends.pop();
+    }
+    
+
+    ///добавить пользователю ареду
+    function setUserArendsEstate(address arendator, uint tokenId, bool status) internal CheckOwnerOfEstate(tokenId) {
+        userArendsEstate[arendator]++;
+        estateArendator[arendator][tokenId] = status;
+        estateRents[tokenId] = status;
+    }
+
+    function getUserArendsEstate(address owner) public view returns(Estate[] memory) {
+        Estate[] memory es = new Estate[](userArendsEstate[owner]);
+        uint p;
+
+        for(uint i = 1; estateId > i; i++) {
+            if(estateArendator[owner][i] == true) {
+                es[p] = estate[i];
+                p++;
+            }
+        }
+
+        return es;
+    }
+
 }
