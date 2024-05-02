d634cde907136934f98710eee4c203f7be636094
Cooper <118792324+irsCooper@users.noreply.github.com>
2024-05-02 13:16:30 +0400

    Update EstNft.sol

--- contracts/EstNft.sol
+++ contracts/EstNft.sol
@@ -6,12 +6,6 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract EstNft is ERC721("EsExNft", "ESEXN") {
 
-    address Owner;
-
-    constructor(address owner) {
-        Owner = owner;
-    }
-    
     function mint(address to, uint tokenId) public {
         _mint(to, tokenId);
     }
@@ -27,37 +21,4 @@ contract EstNft is ERC721("EsExNft", "ESEXN") {
     function OwnerOf(uint tokenId) public view returns(address) {
         return ownerOf(tokenId);
     } 
-
-
-    uint estateId;
-
-    struct Estate { 
-        uint Id;
-        string Address;
-        string Desc;
-        string Haracteristic;
-    }
-
-    mapping (uint => Estate) public estate;
-
-
-
-    function setEstate(string memory _address, string memory _desc, string memory _haract) public {
-        estate[estateId] = Estate(estateId, _address, _desc, _haract);
-        estateId++;
-    }
-
-    function getUserEstate() public view returns(Estate[] memory) {
-        Estate[] memory es = new Estate[](BalanceOf(msg.sender));
-        uint p;
-
-        for(uint i = 0; estateId > i; i++) {
-            if(OwnerOf(i) == msg.sender) {
-                es[p] = estate[i];
-                p++;
-            }
-        }
-
-        return es;
-    }
 }
